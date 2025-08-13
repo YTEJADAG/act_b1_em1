5be26a0e3d454664eb1fbe84a5e5aa8f2de3311e
YeseniaTejada <yeseniatejadagarcia31@gmail.com>
2025-08-12 19:47:55 -0500

    Comentario5

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -9,11 +9,65 @@ public class Main {
         // 3. Modificar algún atributo usando un setter
         // 4. Mostrar la información actualizada
 
+        Estudiante estudiante1 = new Estudiante("Valeria García", 33, "4,5");
+        Estudiante estudiante2 = new Estudiante("Jony García", 30, "3,8");
+
+        
+        System.out.println("Estudiante 1:");
+        estudiante1.mostrarInformacion();
+
+        System.out.println("Estudiante 2:");
+        estudiante2.mostrarInformacion();
+
+    
+        System.out.println("Modificando información del Estudiante 1");
+        estudiante1.setEdad(28);
+        estudiante1.setPromedio("4,8");
+
+        System.out.println("Información Actualizada del Estudiante 1");
+        estudiante1.mostrarInformacion();
+
+        System.out.println("Información del Estudiante 2 (sin cambios)");
+        estudiante2.mostrarInformacion();
+    
+
+
+
         // TODO: Crear método main para:
         // 1. Crear un libro usando el constructor completo
         // 2. Crear un libro usando el constructor con solo titulo y autor
         // 3. Mostrar información de ambos libros
         // 4. Aplicar descuento usando ambas versiones del método
         // 5. Mostrar información actualizada de los libros
+    
+
+        Libro libro1 = new Libro ("Habitos Atómico", "James Clear", 20.30);
+
+        
+        Libro libro2 = new Libro("Harry Potter", "J.K. Rowlings");
+        
+        
+        System.out.println("Información Inicial de Libros");
+        System.out.println("Libro 1:");
+        libro1.mostrarInformacion();
+
+        System.out.println("Libro 2:");
+        libro2.mostrarInformacion();
+
+        
+        System.out.println("Aplicando descuentos");
+        System.out.println("Aplicando descuento del 10% a Libro 1:");
+        libro1.aplicarDescuento(10.0);
+
+        System.out.println("\nAplicando descuento de $5 a Libro 2:");
+        libro2.aplicarDescuento(5);
+
+        
+        System.out.println("Información Actualizada de Libros");
+        System.out.println("Libro 1 (después del descuento):");
+        libro1.mostrarInformacion();
+
+        System.out.println("Libro 2 (después del descuento):");
+        libro2.mostrarInformacion();
     }
 }
